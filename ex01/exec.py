14b619e221835be7f4a30e836c936a1099e611e7
Florian Muller <fmuller@student.42.fr>
2020-03-09 18:10:43 +0100

    ex01 doe

---
+++ ex01/exec.py
@@ -0,0 +1,10 @@
+import sys
+
+def main():
+    string = " ".join(sys.argv[1:]).swapcase()[::-1]
+    print(string)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        main()
+    pass
